3f30c05a19753866b1e13e188da0400bc9dda2b4
nncsumb <127153947+nncsumb@users.noreply.github.com>
2023-12-13 15:13:25 +0000

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -559,7 +559,6 @@ function showBootstrapAlert(message, type = "info") {
   alert.role = "alert";
   alert.innerHTML = `
     ${message}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   `;
 
   alertContainer.appendChild(alert);
